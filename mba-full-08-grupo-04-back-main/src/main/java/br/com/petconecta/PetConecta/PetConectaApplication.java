07d9c3746d7a6d079b70a2d77fd4a759c6cc7e06
Renan Vitorino <61523329+renanvitorino@users.noreply.github.com>
2024-10-29 18:12:26 -0300

    Add files via upload

---
+++ mba-full-08-grupo-04-back-main/src/main/java/br/com/petconecta/PetConecta/PetConectaApplication.java
@@ -0,0 +1,13 @@
+package br.com.petconecta.PetConecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetConectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetConectaApplication.class, args);
+	}
+
+}
---
+++ mba-full-08-grupo-04-back-main/src/test/java/br/com/petconecta/PetConecta/PetConectaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petconecta.PetConecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetConectaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
